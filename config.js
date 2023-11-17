205a7d2ff562b4d7dd9399e8a0159782b172515e
Dornavan <130479724+dorndickence@users.noreply.github.com>
2023-11-17 20:43:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
-  ['919074692450', 'ABHISHEK-SURESH', true], 
+  ['254710770837', 'Dorn Dickence', true],
+  ['254710770837', 'Dornan Dickence', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['254710770837','254710770837'] 
+global.prems = ['254710770837', '254710770837', '919074692450']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = '𝐃𝐨𝐫𝐧-𝐛𝐨𝐭'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🤭𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷' 
-global.author = 'ABHISHEK SURESH' 
+global.packname = '𝐃𝐨𝐫𝐧𝐚𝐯𝐚𝐧🤭𝐃𝐢𝐜𝐤𝐞𝐧𝐜𝐞' 
+global.author = '𝐃𝐎𝐑𝐍 𝐃𝐈𝐂𝐊𝐄𝐍𝐂𝐄' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
 global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
